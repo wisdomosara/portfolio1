9200bc71338b251c43ee3094242b6d07dbd268a6
wisdomosara <50260832+wisdomosara@users.noreply.github.com>
2020-04-15 15:42:21 +0100

    Add files via upload

--- port1.js
+++ port1.js
@@ -1,4 +1,22 @@
-function trans(){
+
+function openNav(){
 let x = document.querySelector("#mobile-button")
-x.classList.toggle("change")
+x.classList.toggle("change");
+
+let nav = document.querySelector("#changebutton")
+nav.classList.toggle("show")
 }
+
+let i = 0
+    var txt1 = "My Name is Wisdom" + "\n"  + "I'm a Web Developer"
+    var speed = 100;
+function typeWriter() {
+    if (i < txt1.length) {
+        document.querySelector(".herotext").innerHTML += txt1.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed)
+    }
+}
+
+
+
